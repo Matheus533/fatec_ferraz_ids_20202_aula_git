ccb3c75f61fcda57ee1f0527e471efa27499d047
Matheus533 <thezo2014@hotmail.com>
2020-09-12 20:45:40 -0300

    ja faz soma e subtração

--- aula.py
+++ aula.py
@@ -1,5 +1,10 @@
-print ("soma")
+print ("soma ou subtração")
+op = int (input ("1-Soma 2-Subtração"))
 a = int (input ("Primeiro operando"))
 b = int (input ("Segundo operando"))
+res = 0
+if op == 1:
 res = a + b
+else:
+res = a - b
 print (res)
